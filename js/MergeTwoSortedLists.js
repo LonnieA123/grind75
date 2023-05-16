2cd2a89f1e87d3ac65931d0327c6a2fefdce1294
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-05-16 12:54:15 -0500

    sorting two arrays of numbers with no concat or sort

---
+++ js/MergeTwoSortedLists.js
@@ -0,0 +1,56 @@
+//Merge two sorted lists
+
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists in a one sorted list. The list should be made by splicing
+// together the nodes of the first two lists.
+
+// Example 2:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+
+
+let list1 = [1,3,5,5,6]
+let list2 = [2,6]
+
+// let mergeTwoSortedLists = function (list1,list2){
+//     let list = list1.concat(list2)
+//     return list.sort()
+// }
+
+let mergeTwoSortedLists = function (list1, list2) {
+    let output = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < list1.length && j < list2.length) {
+        if (list1[i] <= list2[j]) {
+            output.push(list1[i]);
+            i++;
+        } else {
+            output.push(list2[j]);
+            j++;
+        }
+    }
+
+    while (i < list1.length) {
+        output.push(list1[i]);
+        i++;
+    }
+    while (j < list2.length) {
+        output.push(list2[j]);
+        j++;
+    }
+
+    return output;
+}
+
+console.log(mergeTwoSortedLists(list1,list2))
