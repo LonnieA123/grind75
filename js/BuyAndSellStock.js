9a641f7521f3453a13fa46b33c93d5f37fd66e21
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-05-24 17:14:28 -0500

    best time to buy question completed

--- js/BuyAndSellStock.js
+++ js/BuyAndSellStock.js
@@ -1,4 +1,4 @@
-// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
@@ -6,7 +6,18 @@
 
 // Example 1:
 // Input:
-// prices = [7,1,5,3,6,4]
+prices =
+    [    7
+        ,1
+        ,5
+        ,3
+        ,6
+        ,4
+    ]
+
+
+
+
 // Output: 5
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
@@ -17,21 +28,22 @@
 // Output: 0
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
-let maxProfit = function(prices) {
-let maxProfit = 0;
-let minPrice = prices[0];
-    for(let i = 0; i < prices.length; i++){
-        let currentPrice = prices[i]
-        let currentProfit = currentPrice - minPrice
-        if(currentProfit > maxProfit){
-            maxProfit = currentProfit
-        }
-        if (currentPrice < minPrice){
-            minPrice = currentPrice
-        }
-    }
-
-return maxProfit
-};
-
+ var maxProfit = function(prices) {
+     let maxProfit = 0;
+     let minPrice = prices[0];
+     for (let i = 1; i < prices.length; i++) {
+         const currentPrice = prices[i];
+         const currentProfit = currentPrice - minPrice;
+         if (currentProfit > maxProfit) {
+             maxProfit = currentProfit;
+         }
+         if (currentPrice < minPrice) {
+             minPrice = currentPrice;
+         }
+     }
+     return maxProfit;
+ };
 console.log(maxProfit(prices))
+
+
+
