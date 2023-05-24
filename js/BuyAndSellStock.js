9b9b42b380282a4bbd02f6fab4ae1b4d99549c6d
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-05-23 23:45:42 -0500

    test change

--- js/BuyAndSellStock.js
+++ js/BuyAndSellStock.js
@@ -6,7 +6,7 @@
 
 // Example 1:
 // Input:
-prices = [7,1,5,3,6,4]
+// prices = [7,1,5,3,6,4]
 // Output: 5
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
@@ -18,20 +18,20 @@ prices = [7,1,5,3,6,4]
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
 let maxProfit = function(prices) {
-let max = 0;
-let min = prices[0];
+let maxProfit = 0;
+let minPrice = prices[0];
     for(let i = 0; i < prices.length; i++){
         let currentPrice = prices[i]
-        let profit = currentPrice - min
-        if(profit > max){
-            max = profit
+        let currentProfit = currentPrice - minPrice
+        if(currentProfit > maxProfit){
+            maxProfit = currentProfit
         }
-        if (currentPrice < min){
-            min = currentPrice
+        if (currentPrice < minPrice){
+            minPrice = currentPrice
         }
     }
 
-return max
+return maxProfit
 };
 
 console.log(maxProfit(prices))
