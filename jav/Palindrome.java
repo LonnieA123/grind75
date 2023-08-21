191e91e724948f11fbb342326460cb7f2c595aa3
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-08-21 17:20:30 -0500

    FIZZBUZZ IN JAVA

--- jav/Palindrome.java
+++ jav/Palindrome.java
@@ -5,30 +5,30 @@ import java.util.Objects;
 
 public class Palindrome {
 
-    public static Boolean isPalindrome(String input){
-        String rev = new StringBuilder(input).reverse().toString();
-        return input.equalsIgnoreCase(rev);
-    }
+//    public static Boolean isPalindrome(String input){
+//        String rev = new StringBuilder(input).reverse().toString();
+//        return input.equalsIgnoreCase(rev);
+//    }
 
-    public static boolean plaindromty(String input){
+    public static Boolean isPalindrome(String input){
         String[] arr = input.split("");
         String[] rev = new String[input.length()];
         int j = 0;
 
-        for (int i = input.length() - 1; i >= 0; i--){
+        for (int i = 0; i <= input.length() - 1 ; i++){
             rev[j] = arr[i];
             j++;
         }
 
+        String revString = String.join("",rev);
 
-        String revStr = String.join("", rev);
-        return input.equalsIgnoreCase(revStr);
+        return input.equals(revString);
 
     }
 
 
     public static void main(String[] args) {
 
-        System.out.println(plaindromty("raceCar"));
+        System.out.println(isPalindrome("raceCar"));
     }
 }
