c78976e019ab51f8d2612af08f05620e5062ec8f
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-09-14 14:27:53 -0500

    Twosum test change

--- jav/TwoSum.java
+++ jav/TwoSum.java
@@ -52,7 +52,7 @@ public class TwoSum {
         int[] array2 = {3,3};
         int[] array3 = {3,2,4};
 
-        System.out.println(Arrays.toString(tw0Sum(array2, 9)));
+        System.out.println(Arrays.toString(tw0Sum(array3, 9)));
 
     }
 }
