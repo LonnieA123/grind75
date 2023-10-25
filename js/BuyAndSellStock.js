f1c4706359c46a36eaa61500aae456da9e4ba057
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-10-25 15:11:41 -0500

    test changes

--- js/BuyAndSellStock.js
+++ js/BuyAndSellStock.js
@@ -20,7 +20,7 @@
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
      prices =
-         [    7
+         [    6
              ,1
              ,5
              ,4
